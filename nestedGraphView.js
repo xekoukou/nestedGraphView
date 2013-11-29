2f702dc32ede9998f83f13f32b199256516ee5e7
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2013-11-29 20:22:33 +0000

    first commit

---
+++ nestedGraphView.js
@@ -0,0 +1,37 @@
+var posX;
+var posY;
+var zoom; //the zoom level
+var level;
+
+
+function node(){
+
+
+
+}
+
+function getData(x,newX,y,newY, level){
+
+}
+
+function onRightKey(){
+
+
+getData(posX,posX+zooom,posY,posY, level);
+
+
+
+posX = zoom + posX;
+}
+
+function onLeftKey(){
+
+}
+
+function onUpKey(){
+
+}
+
+function onDownKey() {
+
+}
