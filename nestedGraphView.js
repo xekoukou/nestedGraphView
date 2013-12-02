e8a638b410349e0c0e5c7b13397db0ece2008c43
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2013-12-02 19:56:46 +0000

    more changes

--- nestedGraphView.js
+++ nestedGraphView.js
@@ -1,37 +1,105 @@
-var posX;
-var posY;
-var zoom; //the zoom level
-var level;
+//zoom , the zoom level in centimeters per real centimeter
+// level, the level of abstraction or nest
 
+// window.onLoad(alert("lol");
 
-function node(){
+function node(id, parentId, level, childrenIds , summary , content , posX, posY){
 
-
+this.id = id;
+this.parentId = parentd;
+this.childrenIds = childrenIds;
+this.posX = posX; 
+this.posY = posY;
+this.content = '<div class = "nestedGraphNodeContent '+id+'" >' + content + '</div>';
+this.summary = '<div class = "nestedGraphNode '+id+'" >' + summary + '</div>';
 
 }
 
-function getData(x,newX,y,newY, level){
+function data(){
+
+this.nodes;
+
+this.getdiff = function(x,newX,y,newY, level){
+
+};
+
+this.getLevel = function(x,y,level){
 
 }
 
-function onRightKey(){
+}
+
+function View(posX,posY,zoom,level,id_connector,width,height){
+
+this.zoom = zoom;
+this.level = level;
+this.data = new data();
 
 
+this.width =width;
+this.height = height;
+this.rootId = "#"+id_connector;
+
+this.render = function(){
+
+(rootId).empty();
+
+for(i = 0; i++; i < this.data.nodes.length){
+
+var node = this.data.nodes[i];
+$(rootId).append(node.summary);
+$(rootId).append(node.Content);
+$('.'+node.id+'.nestedGraphNode').css('bottom',((node.posY - this.posY)/zoom).toString()+'px');
+$('.'+node.id+'.nestedGrapgNode').css('left',((node.posX - this.posX)/zoom).toString()+'px');
+$('.'+node.id+'.nestedGraphNodeContent').css('bottom',((node.posY -this.posY +20)/zoom).toString()+'px');
+$('.'+node.id+'.nestedGrapgNodeContent').css('left',((node.posX - this.posX)/zoom).toString()+'px');
+
+}
+
+};
+
+this.onZoomChange = function(zoom){
+this.zoom = zoom;
+this.render();
+}
+
+this.onLevelChange = function(level){
+this.level = level;
+this.data.getLevel(posX,posY, level);
+
+this.render();
+};
+
+
+this.posX = posX;
+this.posY = posY;
+
+this.onLeftKey = funcion(){
+this.data.getDiff(posX,posX-zooom,posY,posY, level);
+posX = posX-zoom;
+
+this.render();
+};
+
+this.onRightKey = funcion(){
 getData(posX,posX+zooom,posY,posY, level);
+posX = posX + zoom;
 
+this.render();
+};
 
+this.onDownKey = funcion(){
+getData(posX,posX,posY,posY-zoom, level);
+posY = posY - zoom;
 
-posX = zoom + posX;
-}
+this.render();
+};
 
-function onLeftKey(){
+this.onUpKey = function(){
+getData(posX,posX,posY,posY + zoom, level);
+posY = posY + zoom;
 
-}
-
-function onUpKey(){
-
-}
-
-function onDownKey() {
+this.render();
+};
 
 }
